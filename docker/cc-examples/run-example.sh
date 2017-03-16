baf686685b7b43e53c96d344eb4958584731dd91
fcisneros <fernando@datank.ai>
2017-03-16 16:54:14 -0600

    Added S3 access capabilities

--- docker/cc-examples/run-example.sh
+++ docker/cc-examples/run-example.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-DATADIR="/app/data"
 JAR="/app/cc-warc-examples.jar"
-
-export PATH="/opt/hadoop/bin:${PATH}"
 
 function usage() {
 cat <<EOF
 
 Run one of the CommonCrawl examples (https://github.com/fcisneros/cc-warc-examples) jobs.
 
-Usage: $0 [example]
+Usage: $0 [exampleName] inputPath outputPath
 
-examples:
-wc        WETWordCount example
-st        WATServerType example
+exampleName values:
+wc            WETWordCount example
+st            WATServerType example
+
+inputPath:    The example inputPath (ie file:///data/*warc.wet.gz)
+outputPath:   The example local outputPath (ie /data/output)
+
 
 EOF
 exit 1
 }
 
 if [[ $1 == "wc" ]]; then
-  inputPath="file://${DATADIR}/*.warc.wet.gz"
-  #inputPath="s3://commoncrawl/crawl-data/CC-MAIN-2013-48/segments/1386163035819/wet/CC-MAIN-20131204131715-00000-ip-10-33-133-15.ec2.internal.warc.wet.gz"
-  #inputPath="s3://commoncrawl/crawl-data/CC-MAIN-2013-48/segments/1386163035819/wet/*.warc.wet.gz"
-  outputPath="${DATADIR}/wet-wordcount"
+  inputPath="${2}"
+  outputPath="${3}/wet-wordcount"
 
   hadoop jar $JAR org.commoncrawl.examples.mapreduce.WETWordCount -Dinput=$inputPath -Doutput=$outputPath && \
   mkdir -p $outputPath && rm -f $outputPath/part* && \
@@ -34,10 +33,8 @@ if [[ $1 == "wc" ]]; then
 fi
 
 if [[ $1 == "st" ]]; then
-  inputPath="file://${DATADIR}/*.warc.wat.gz"
-  #inputPath="s3://commoncrawl/crawl-data/CC-MAIN-2013-48/segments/1386163035819/wet/CC-MAIN-20131204131715-00000-ip-10-33-133-15.ec2.internal.warc.wet.gz"
-	#inputPath="s3://commoncrawl/crawl-data/CC-MAIN-2013-48/segments/1386163035819/wet/*.warc.wet.gz"
-  outputPath="${DATADIR}/wat-servertype"
+  inputPath="${2}"
+  outputPath="${3}/wat-servertype"
 
   hadoop jar $JAR org.commoncrawl.examples.mapreduce.WATServerType -Dinput=$inputPath -Doutput=$outputPath && \
   mkdir -p $outputPath && rm -f $outputPath/part* && \
